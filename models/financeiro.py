2b8cbaba291381eceda4bc3e7352a2103c4cf8ef
carlos-edu2367 <contato.carlosedu2367@gmail.com>
2025-06-13 08:30:16 -0300

    Tabelas de contas a pagar/receber

--- models/financeiro.py
+++ models/financeiro.py
@@ -26,13 +26,56 @@ def criar_tabelas_financeiro():
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS contas_a_pagar(
                 id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+                empresa_id INTEGER NOT NULL,
                 responsavel_id INTEGER NOT NULL,
                 titulo_conta TEXT NOT NULL,
                 descrição TEXT NOT NULL,
                 fornecedor TEXT NOT NULL,
-                valor TEXT NOT NULL,
+                valor DECIMAL NOT NULL,
                 data_vencimento DATE NOT NULL,
                 data_pagamento DATE,
-                comprovante_ref TEXT
+                comprovante_ref TEXT,
+                nota_fiscal_ref TEXT
             )
+    """)
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS contas_a_receber(
+            id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+            empresa_id INTEGER NOT NULL,
+            responsavel_id INTEGER NOT NULL,
+            titulo_conta TEXT NOT NULL,
+            descrição TEXT NOT NULL,
+            valor DECIMAL NOT NULL,
+            data_vencimento DATE NOT NULL,
+            data_pagamento DATE,
+            comprovante_ref TEXT,
+            nota_fiscal_ref TEXT
+        )
+    """)
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS contas_recorrentes(
+            id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+            empresa_id INTEGER NOT NULL,
+            titulo TEXT NOT NULL,
+            descrição TEXT NOT NULL,
+            dia_vencimento TEXT NOT NULL,
+            valor DECIMAL NOT NULL
+        )
+    """)
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS relação_contas_recorrentes(
+            id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+            id_conta INTEGER NOT NULL,
+            empresa_id INTEGER NOT NULL,
+            titulo TEXT NOT NULL,
+            descrição TEXT NOT NULL,
+            valor DECIMAL NOT NULL,
+            data_vencimento DATE NOT NULL,
+            data_pagamento DATE,
+            comprovante_ref TEXT,
+            nota_fiscal_ref TEXT
+        )
     """)
